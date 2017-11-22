77cf50e693cde3fb470564b1219b9d93af63d2ed
Will Toohey <will@mon.im>
2017-11-23 00:06:38 +1000

    Version 3.3

--- src/js/HuesCore.js
+++ src/js/HuesCore.js
@@ -112,7 +112,7 @@ class HuesCore {
         };
 
         // Bunch-o-initialisers
-        this.version = 32;
+        this.version = 33;
         this.versionStr = (this.version/10).toFixed(1);
         this.versionHex = this.version.toString(16);
         this.beatIndex = 0;
