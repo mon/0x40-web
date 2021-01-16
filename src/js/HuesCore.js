a8cb57928a9547b4ea0e72ea3c7e0a92267cd58d
Will Toohey <will@mon.im>
2021-01-16 19:03:26 +1000

    Version 3.5

--- src/js/HuesCore.js
+++ src/js/HuesCore.js
@@ -109,7 +109,7 @@ class HuesCore {
         };
 
         // Bunch-o-initialisers
-        this.version = 34;
+        this.version = 35;
         this.versionStr = (this.version/10).toFixed(1);
         this.versionHex = this.version.toString(16);
         this.beatIndex = 0;
