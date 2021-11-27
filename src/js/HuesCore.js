ffd6cf67f95b9505bf846e283c640c8e4b5c1630
Will Toohey <will@mon.im>
2021-11-27 18:34:53 +1000

    Bump ver to 4.1

--- src/js/HuesCore.js
+++ src/js/HuesCore.js
@@ -123,7 +123,7 @@ class HuesCore {
         };
 
         // Bunch-o-initialisers
-        this.version = 40;
+        this.version = 41;
         this.versionStr = (this.version/10).toFixed(1);
         this.versionHex = this.version.toString(16);
         this.beatIndex = 0;
